e053781a3950168b8f55d451538af69fc6182ad5
Daniel Gabay <gabaydaniel95@gmail.com>
2023-07-23 21:01:09 +0300

    Updated final

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState,useEffect } from "react";
+import React, { useContext, useState } from "react";
 
 import { Container, Row, Col, Button, Form, Card } from "react-bootstrap";
 import { AuthContext } from "../context/authContext";
@@ -14,7 +14,7 @@ export default function Profile(props) {
   const [email, setEmail] = useState(userData?.email || "");
   const [password, setPassword] = useState(userData?.password || "");
   const [imgUrl, setImgUrl] = useState(userData?.imgUrl || "");
-  // const [formData, setFormData] = useState(new FormData());
+  const [preImg, setPreImg] = useState("");
 
 
   const handleNameChange = (e) => setName(e.target.value);
@@ -22,7 +22,8 @@ export default function Profile(props) {
   const handlePassowrdChange = (e) => setPassword(e.target.value);
   const handleImageChange = (e) => {
     const file = e.target.files[0];
-    setImgUrl(URL.createObjectURL(file));
+    setImgUrl(file);
+    setPreImg(URL.createObjectURL(file))
   };
 
   const handleSaveChanges = async () => {
@@ -32,27 +33,27 @@ export default function Profile(props) {
     formData.append("password", password);
     formData.append("imgUrl", imgUrl);
   console.log(...formData);
-    // try {
-    //   const token = getJWT();
-    //   const reqHeaders = {
-    //     "x-auth-token": token,
-    //     "Content-Type": "multipart/form-data",
-    //   };
-    //   await http.patch(
-    //     `http://localhost:5000/api/users/${userData?._id}`,
-    //     formData,
-    //     { headers: reqHeaders }
-    //   );
-    //   console.log(formData);
-    //   console.log(`Updating ${userData?.name} user has been success !`);
-    //   // setTimeout(() => {
-    //   //   toast.success("המשתמש עודכן בהצלחה");
-    //   //   window.location = "/UserDashboard";
-    //   // }, 2000);
-    // } catch (error) {
-    //   console.log(error);
-    //   toast.error("אירעה שגיאה בעת עדכון המשתמש");
-    // }
+    try {
+      const token = getJWT();
+      const reqHeaders = {
+        "x-auth-token": token,
+        "Content-Type": "multipart/form-data",
+      };
+      await http.patch(
+        `http://localhost:5000/api/users/${userData?._id}`,
+        formData,
+        { headers: reqHeaders }
+      );
+      console.log(formData);
+      console.log(`Updating ${userData?.name} user has been success !`);
+      setTimeout(() => {
+        toast.success("המשתמש עודכן בהצלחה");
+        window.location = "/UserDashboard";
+      }, 2000);
+    } catch (error) {
+      console.log(error);
+      toast.error("אירעה שגיאה בעת עדכון המשתמש");
+    }
   };
   
   
@@ -72,8 +73,8 @@ export default function Profile(props) {
                         <Card style={{ width: "18rem", marginBottom: "20px" }}>
                           <Card.Img
                             variant="top"
-                            src={imgUrl ? imgUrl : userData?.imgUrl}
-                            alt="pre image"
+                            src={preImg ? preImg : userData?.imgUrl}
+                            alt="Pre image"
                           />
                         </Card>
                       )}
